2512cc3ee0a45e2a9774b1757b301a3fb2369986
SIDIDEV <d3v@192.168.1.5>
2023-02-12 14:15:49 +0100

    vue click event et condition v-if v-show avec vue 3

--- Formation vuejs/app.js
+++ Formation vuejs/app.js
@@ -2,12 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             name: 'Ayaline',
-            year: 2023
+            year: 2023,
+            showContent: false
         }
     },
     methods: {
         changeName(text) {
             this.name = text
+        },
+        toggleShowContent(){
+            this.showContent = !this.showContent
         }
     }
 })
